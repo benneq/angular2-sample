e48af8e3d701e07d5f909ce9a3cbdcbe8d6aebcb
Benjamin Makus <benjamin.makus@me.com>
2016-03-10 00:31:17 +0100

    form compatible query directive

--- app/common/directive/query.directive.ts
+++ app/common/directive/query.directive.ts
@@ -1,38 +1,42 @@
-import {Directive, EventEmitter, Input, Output} from 'angular2/core'
-import {NgModel} from 'angular2/common';
-import {Observable} from 'rxjs/Observable';
+import {Directive, Renderer, ElementRef, forwardRef, Provider, Input} from 'angular2/core';
+import {Observable} from 'rxjs/Rx'
+import {NG_VALUE_ACCESSOR, DefaultValueAccessor} from 'angular2/common';
+import {CONST_EXPR} from 'angular2/src/facade/lang';
+
+const PROVIDER = CONST_EXPR(new Provider(NG_VALUE_ACCESSOR, {useExisting: forwardRef(() => QueryDirective), multi: true}));
 
 @Directive({
-    selector: '[queryDirective][ngModel]'
+    selector: '[query]',
+    bindings: [PROVIDER]
 })
-export class QueryDirective {
+export class QueryDirective extends DefaultValueAccessor {
     @Input() timeout: number = 250;
     @Input() omitEqual: boolean = true;
     @Input() omitMultiWhitespace: boolean = true;
     @Input() trim: boolean = true;
-    
-    constructor(public model:NgModel) {
-        
+
+    constructor(_renderer: Renderer, private el: ElementRef) {
+        super(_renderer, el);
     }
     
     ngOnInit() {
-        var o: Observable<string> = this.model.control.valueChanges
-            .debounceTime(this.timeout);
-        
+        var o:Observable<string> = Observable.fromEvent(this.el.nativeElement, 'keyup')
+            .map(val => this.el.nativeElement.value)
+            .debounceTime(this.timeout)
+            
         if(this.omitMultiWhitespace) {
-            o = o.map(val => val.replace(/ +(?= )/g,''));
+            o = o.map((val: string) => val.replace(/ +(?= )/g,''));
         }
         
         if(this.trim) {
-            o = o.map(val => val.trim());
+            o = o.map((val: string) => val.trim());
         }
         
         if(this.omitEqual) {
-            o = o.filter(val => val != this.model.value);
+            o = o.distinctUntilChanged();
         }
         
-        o.subscribe(val => {
-            this.model.control.updateValue(val, {emitEvent:true});
-        });
+        o.subscribe(val => this.onChange(val));
     }
-}
+
+} 
